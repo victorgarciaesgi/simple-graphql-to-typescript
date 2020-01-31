7ad8ad495c8d8777902ae2b76f1613acb271f3fd
Victor GARCIA <victor@totem.paris>
2020-01-31 17:33:30 +0100

    fixed!

--- __tests__/global.spec.ts
+++ __tests__/global.spec.ts
@@ -1,20 +1,18 @@
 import { sgtsGenerate } from '../src';
 
 jest.setTimeout(10000);
-it('Generates types correctly from jsonplaceholder', async done => {
+it('Generates types correctly from jsonplaceholder', async () => {
   const result = await sgtsGenerate({
     endpoint: 'https://json-placeholder-graphql.herokuapp.com/graphql',
     output: './debug/__generated.ts',
   });
   expect(result).toMatchSnapshot();
-  done();
 });
 
-it('Generates types correctly from graphqlZero', async done => {
+it('Generates types correctly from graphqlZero', async () => {
   const result = await sgtsGenerate({
     endpoint: 'https://graphqlzero.almansi.me/api',
     output: './debug/__generated.ts',
   });
   expect(result).toMatchSnapshot();
-  done();
 });
