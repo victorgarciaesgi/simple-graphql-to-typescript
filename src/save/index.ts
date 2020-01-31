31e6b4284a0a569568b2757ed628fa3af75403f7
Victor GARCIA <victor@totem.paris>
2020-01-31 16:25:34 +0100

    Fixed file save

--- src/save/index.ts
+++ src/save/index.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import * as ts from 'typescript';
 import chalk from 'chalk';
+import mkdirp from 'mkdirp';
 
 const saveModels = ora('Saving models file...');
 
@@ -14,7 +15,7 @@ export function saveFile(
   json?: boolean
 ): Promise<string> {
   saveModels.start();
-  return new Promise((res, rej) => {
+  return new Promise(async (res, rej) => {
     try {
       const formatedModelsFile = prettier.format(template, {
         config: path.resolve(__dirname, '../.prettierrc'),
@@ -25,45 +26,16 @@ export function saveFile(
         parser: 'typescript',
       });
       const outputfile = path.resolve(process.cwd(), output);
-
-      fs.writeFile(outputfile, formatedModelsFile || template, err => {
-        if (err) {
-          saveModels.fail('Saving models file failed');
-          console.log(err.message);
-          return rej('Error in saving file');
-        } else {
-          saveModels.succeed(`🗃 Typescript models saved at ${chalk.bold(`${output}`)}`);
-          if (jsMode) {
-            try {
-              TypescriptCompile([outputfile], {
-                module: ts.ModuleKind.CommonJS,
-                target: ts.ScriptTarget.ESNext,
-                moduleResolution: ts.ModuleResolutionKind.NodeJs,
-                allowSyntheticDefaultImports: true,
-                experimentalDecorators: true,
-                emitDecoratorMetadata: true,
-                resolveJsonModule: true,
-                esModuleInterop: true,
-                removeComments: true,
-                noImplicitAny: false,
-                noUnusedLocals: false,
-                pretty: true,
-                sourceMap: false,
-                downlevelIteration: true,
-                declaration: true,
-                skipLibCheck: true,
-                types: ['node'],
-              });
-              fs.unlinkSync(outputfile);
-              res(formatedModelsFile);
-            } catch (e) {
-              rej(e);
-            }
-          } else {
-            res(formatedModelsFile);
-          }
-        }
-      });
+      if (fs.existsSync(outputfile)) {
+        console.log(outputfile);
+        await writeOutput(outputfile, formatedModelsFile, jsMode);
+      } else {
+        let dirList = outputfile.split('/');
+        dirList.pop();
+        const dirPath = dirList.join('/');
+        await mkdirp(dirPath);
+        await writeOutput(outputfile, formatedModelsFile, jsMode);
+      }
     } catch (e) {
       saveModels.fail('Saving models file failed');
       return rej(e);
@@ -74,4 +46,44 @@ export function saveFile(
 function TypescriptCompile(fileNames: string[], options: ts.CompilerOptions): void {
   let program = ts.createProgram(fileNames, options);
   program.emit();
+}
+
+async function writeOutput(path: string, content: string, jsMode?: boolean) {
+  try {
+    await fs.writeFileSync(path, content);
+    saveModels.succeed(`🗃 Typescript models saved at ${chalk.bold(`${path}`)}`);
+    if (jsMode) {
+      try {
+        TypescriptCompile([path], {
+          module: ts.ModuleKind.CommonJS,
+          target: ts.ScriptTarget.ESNext,
+          moduleResolution: ts.ModuleResolutionKind.NodeJs,
+          allowSyntheticDefaultImports: true,
+          experimentalDecorators: true,
+          emitDecoratorMetadata: true,
+          resolveJsonModule: true,
+          esModuleInterop: true,
+          removeComments: true,
+          noImplicitAny: false,
+          noUnusedLocals: false,
+          pretty: true,
+          sourceMap: false,
+          downlevelIteration: true,
+          declaration: true,
+          skipLibCheck: true,
+          types: ['node'],
+        });
+        fs.unlinkSync(path);
+        Promise.resolve(content);
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    } else {
+      return Promise.resolve(content);
+    }
+  } catch (e) {
+    saveModels.fail('Saving models file failed');
+    console.log(e.message);
+    return Promise.reject('Error in saving file');
+  }
 }
